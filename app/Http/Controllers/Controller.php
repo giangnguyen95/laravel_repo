51549da0ae9a9541b6dc02cc65140712fc926e7d
giangnguyen95 <nguyenthigiang95bn@gmail.com>
2016-07-20 17:00:03 +0700

    first commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,8 +7,10 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+use App\User;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+    //$a = User::where(;;;)
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,44 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+use App\Task;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+        return redirect('/index')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/index');
+});
+
+Route::delete('/task/{task}', function (Task $task) {
+        $task->delete();
+
+        return redirect('/index');
+});
+
+Route::get('/index', function(){
+	$tasks = Task::orderBy('created_at','asc')->get();
+	return view('tasks',['tasks'=>$tasks]);
+});
+
+Route::get('task/select', function(){
+	$data = App\Task::all()->take(5)->toArray();
+	echo '<pre>';
+	print_r($data);
+	echo '</pre>';
 });
